aa7590831bbb13d82c0ad3bfaf60ba97647e4b83
Juan Dorado <jfdoradotr@icloud.com>
2022-06-01 23:36:22 -0500

    Fix identation and get the same dependencies as sundell repo

--- Sources/XcodeOneDarkTheme/XcodeOneDarkTheme.swift
+++ Sources/XcodeOneDarkTheme/XcodeOneDarkTheme.swift
@@ -1,6 +1,6 @@
 public struct XcodeOneDarkTheme {
-    public private(set) var text = "Hello, World!"
+  public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+  public init() {
+  }
 }
